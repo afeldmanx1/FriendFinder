2f97fae5cb55f2b111b287782158c49ba62e785e
afeldmanx1 <alexf0922@gmail.com>
2019-02-21 17:53:46 -0500

    stable version

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+    {
+        name: "Peter",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/Peter_Griffin.png/220px-Peter_Griffin.png",
+        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    },
+    {
+        name: "Stewie",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/0/02/Stewie_Griffin.png/220px-Stewie_Griffin.png",
+        scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+    },
+    {
+        name: "Meg",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/Meg_Griffin.png/185px-Meg_Griffin.png",
+        scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+    },
+    {
+        name: "Lois",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a5/Lois_Griffin.png/170px-Lois_Griffin.png",
+        scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+    },
+    {
+        name: "Brian",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Brian_Griffin.png/195px-Brian_Griffin.png",
+        scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    }
+];
+
+module.exports = friends;
